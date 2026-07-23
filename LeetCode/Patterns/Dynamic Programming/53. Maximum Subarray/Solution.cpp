ceb85362f7aba9b279cc69140f33c6990acf7b01
Dhitee Shah <dhitee018@gmail.com>
2026-07-23 08:33:02 +0530

    Add C++ Solution to Patterns/Dynamic Programming

---
+++ LeetCode/Patterns/Dynamic Programming/53. Maximum Subarray/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int maxSum= nums[0];
+        int sum=0;
+
+        for (int i=0;i<n;i++){
+             sum+= nums[i];
+            if (sum> maxSum){
+                 maxSum=sum;}
+            if (sum< 0){
+                sum=0;
+            }
+
+            }
+            return maxSum;
+        }
+    };
